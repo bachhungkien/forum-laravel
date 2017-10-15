0098c7dfb6e5bc057c4c30c1b42a274ebdd979dd
bachhungkien <bachhungkien@gmail.com>
2017-10-15 17:19:58 +0700

    Config database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_7544b146264de1f'),
+            'username' => env('DB_USERNAME', 'bae0aa73ad3a4c'),
+            'password' => env('DB_PASSWORD', '50491b98'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
