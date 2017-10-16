f1ebc3bd1e85927014d093d1f2da7d45f12bb1ce
bachhungkien <bachhungkien@gmail.com>
2017-10-16 07:44:20 +0700

    Create Thread, Reply

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,6 +13,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
+
     static $password;
 
     return [
@@ -20,5 +21,32 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function ($faker) {
+
+    return [
+        'user_id' => function () {
+
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(App\Reply::class, function ($faker) {
+
+    return [
+        'thread_id' => function () {
+
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function () {
+
+            return factory('App\User')->create()->id;
+        },
+        'body' => $faker->paragraph
     ];
 });
