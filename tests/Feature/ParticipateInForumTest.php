94305b167c4bcd5bd741881b52955f7288f94422
bachhungkien <bachhungkien@gmail.com>
2017-10-16 15:42:25 +0700

    Test Validation Errors

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -21,14 +21,27 @@ class ParticipateInForumTest extends TestCase {
 
     public function test_an_authenticated_user_may_participate_in_forum_threads() {
 
-        $this->be($user = factory('App\User')->create());
+        $this->signIn();
 
-        $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->make();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply');
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    public function test_a_reply_requires_a_body() {
+
+        $this->expectException('Illuminate\Validation\ValidationException');
+
+        $this->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
