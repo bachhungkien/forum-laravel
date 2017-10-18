19237660076ee64affb6c707dca563250f60973a
bachhungkien <bachhungkien@gmail.com>
2017-10-18 18:50:43 +0700

    Vue Favorite Component

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -12,6 +12,14 @@ use Illuminate\Database\Eloquent\Model;
 
 trait Favoritable {
 
+    protected static function bootFavoritable() {
+
+        static::deleting(function ($model) {
+
+            $model->favorites->each->delete();
+        });
+    }
+
     public function favorites() {
 
         return $this->morphMany(Favorite::class, 'favorited');
@@ -26,9 +34,21 @@ trait Favoritable {
         }
     }
 
+    public function unfavorite() {
+
+        $attributes = ['user_id' => auth()->id()];
+
+        $this->favorites()->where($attributes)->get()->each->delete();
+    }
+
     public function isFavorited() {
 
         return !!$this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    public function getIsFavoritedAttribute() {
+
+        return $this->isFavorited();
     }
 
     public function getFavoritesCountAttribute() {
